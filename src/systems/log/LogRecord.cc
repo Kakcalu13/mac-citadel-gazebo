b2ddcb17c280f9e9b0e685add6d1927a83c1f977
kakcalu13 <kevin.a.araujo@gmail.com>
2026-04-09 23:30:39 +0530

    updated to fix the string fixes

--- src/systems/log/LogRecord.cc
+++ src/systems/log/LogRecord.cc
@@ -286,7 +286,7 @@ bool LogRecordPrivate::Start(const std::string &_logPath,
 
   // Use directory basename as topic name, to be able to retrieve at playback
   std::string sdfTopicStr = "/" + common::basename(this->logPath) + "/sdf";
-  this->sdfPub = this->node.Advertise(sdfTopicStr, this->sdfMsg.GetTypeName());
+  this->sdfPub = this->node.Advertise(std::string(std::string(sdfTopicStr)), this->sdfMsg.GetTypeName());
 
   // TODO(louise) Combine with SceneBroadcaster's state topic
   std::string stateTopic = "/world/" + this->worldName + "/changed_state";
