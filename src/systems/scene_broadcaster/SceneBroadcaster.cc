704b1691a21e0d0a0e6f8f8ca4a02dbec7631576
kakcalu13 <kevin.a.araujo@gmail.com>
2026-04-15 20:03:55 +0530

    Log request callbacks and responses to help debug state_async communication issues

--- src/systems/scene_broadcaster/SceneBroadcaster.cc
+++ src/systems/scene_broadcaster/SceneBroadcaster.cc
@@ -342,7 +342,11 @@ void SceneBroadcaster::PostUpdate(const UpdateInfo &_info,
     {
       for (const auto &reqSrv : this->dataPtr->stateRequests)
       {
-        this->dataPtr->node->Request(reqSrv, this->dataPtr->stepMsg);
+        ignerr << "SceneBroadcaster: sending state to callback=" << reqSrv
+               << " entities=" << this->dataPtr->stepMsg.state().entities_size()
+               << "\n";
+        bool ok = this->dataPtr->node->Request(reqSrv, this->dataPtr->stepMsg);
+        ignerr << "SceneBroadcaster: node->Request(" << reqSrv << ") returned " << ok << "\n";
       }
       this->dataPtr->stateRequests.clear();
     }
@@ -591,6 +595,8 @@ bool SceneBroadcasterPrivate::SceneInfoService(msgs::Scene &_res)
 void SceneBroadcasterPrivate::StateAsyncService(
     const msgs::StringMsg &_req)
 {
+  ignerr << "SceneBroadcaster::StateAsyncService: got request, callback="
+         << _req.data() << "\n";
   std::unique_lock<std::mutex> lock(this->stateMutex);
   this->stateServiceRequest = true;
   this->stateRequests.insert(_req.data());
